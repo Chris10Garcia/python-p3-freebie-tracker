0ce861e4a7b8940f1a4488276d5ed08fe02b2ed6
Christian Garcia <bones192nd@gmail.com>
2023-07-05 20:35:49 -0400

    got seeding working as intended

--- lib/seed.py
+++ lib/seed.py
@@ -20,3 +20,27 @@ if __name__ == '__main__':
 
     fake = Faker()
 
+    
+    companies = [Company(name=fake.company(), founding_year=random.randint(1980,2022)) for i in range(25)]
+
+    devs = [Dev(name = fake.name()) for i in range(50)]
+
+    freebies = [Freebie(item_name = fake.job(), value = random.randint(1,25)) for i in range(100)]
+
+    session.add_all(companies + devs + freebies)
+    session.commit()
+
+    for dev in devs:
+        for i in range(random.randint(1,8)):
+            company = random.choice(companies)
+            if dev not in company.devs:
+                company.devs.append(dev)
+                session.add(company)
+                session.commit()
+            
+    for freebie in freebies:
+        freebie.company = random.choice(companies)
+        freebie.dev = random.choice(devs)
+
+    session.add_all(freebies)
+    session.commit()
